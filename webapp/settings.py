5134cf6460fd1d13c7855381ed6f2ed38c4766e2
e ckm <eckm0001@gmail.com>
2024-06-24 17:27:13 -0500

    db

--- webapp/settings.py
+++ webapp/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'app' / 'data' / 'output' / 'master.sqlite3',
     }
 }
 
